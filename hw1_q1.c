5031bde1e1ef4395ba7715b6438d465a61c1c08b
mjmaroti <mjmaroti@mtu.edu>
2024-09-09 16:56:34 -0400

    Create hw1_q1.c

---
+++ hw1_q1.c
@@ -0,0 +1,24 @@
+// // Class; CS1111
+// // Date 9/9/2024
+#include <stdio.h>
+int main(void)
+{
+        double op1;
+
+        double op2;
+
+        char op;
+
+        printf("Enter an integer expression:");
+
+        scanf("%d %c %d", &op1,&op,&op2);
+
+        printf("Operand 1: %d\n", op1);
+
+        printf("Operand 2: %d\n", op2);
+
+        printf("Operand: %c\n", op);
+
+        return 0;
+
+}
